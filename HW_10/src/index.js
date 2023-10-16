dd9ea7200fc046cf69e85e1cdf2a8af4cbf689d1
Oleksii Haiduk <oleksiihaiduk@curiosity-2.local>
2023-10-16 12:32:07 +0300

    HomeWork_10

---
+++ HW_10/src/index.js
@@ -0,0 +1,53 @@
+/* Умова №1
+Вам необхідно використовувати масив нотифікацій з минулих занять. 
+До отриманого під час групування об'єктаnotifications, вам необхідно додати ітератор, щоб під час перебору в цикліfor ofми отримували кожен елемент із вкладених списків об'єктаnotificationsтаким чином, немов працюємо з "плоским" масивом.
+*/
+function groupNotifications(notifications) {
+    const groupedNotifications = {};
+        for (const notification of notifications) {
+             const source = notification.source;
+             const text = notification.Text;
+             const date = notification.Date;
+        if (!groupedNotifications[source]) {
+             groupedNotifications[source] = [];
+        }                  
+             groupedNotifications[source].push({Text:text, Date:date});
+        }
+        return groupedNotifications;
+}
+
+const notifications = [
+    { source: 'Email', Text: 'Ви отримали нового листа на електронну пошту.', Date: '11.10.2023' },
+    { source: 'SMS', Text: 'Нове смс повідомлення', Date: '11.10.2023' },
+    { source: 'Telegram', Text: 'Нове повідомлення в Телеграм', Date: '11.10.2023' },
+    { source: 'Viber', Text: 'Вам надійшло повідомлення у Viber', Date: '11.10.2023' },
+];
+
+const grouped = groupNotifications(notifications);
+console.log(grouped);
+
+/* Умова №2
+Вам необхідно реалізувати функціюmemoize(fn), яка приймає вхід функцію і додає їй можливість кешування результатів виконання, щоб уникнути повторних обчислень.
+Це означає, що в разі, коли функція викликається з однаковими параметрами, то результат необхідно брати з кешу. (Тільки примітиви у параметрах та використовуйтеMap)
+*/
+function memoize(fn) {
+    const cache = new Map();
+        return function (...args) {
+    const key = JSON.stringify(args);
+        if (cache.has(key)) {
+        return cache.get(key);
+        } else {
+    const result = fn(...args);
+    cache.set(key, result);
+        return result;        
+            
+        }        
+    };
+}
+function expensiveFunction(a, b) {
+    console.log("Викликаємо кеш");
+        return a + b; 
+}
+    const memoizedExpensiveFunction = memoize(expensiveFunction);
+console.log(memoizedExpensiveFunction(2, 3));
+console.log(memoizedExpensiveFunction(2, 3));    
